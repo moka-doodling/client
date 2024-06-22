96f76edc89f513a83dbf3e3c51f96df5838fad2f
wlgus253254 <wlgus253254@ewhain.net>
2024-06-22 17:10:55 +0900

    :hammer: Fix : 공모전 종료

--- src/pages/AdminRelayList/index.jsx
+++ src/pages/AdminRelayList/index.jsx
@@ -16,18 +16,30 @@ import { axiosInstance } from '../../apis';
 const AdminRelayList = () => {
     const [items_relay, setItemsRelay] = useState([]);
 
-    useEffect(() => {
-        const fetchRelays = async() => {
-            try {
-                const response = await axiosInstance.get('/admin/relay/list');
-                console.log('response : ' + response);
-                setItemsRelay(response.data);
-            } catch (error) {
-                console.error('Error fetching relay : ', error);
-            }
+    const fetchRelays = async() => {
+        try {
+            const response = await axiosInstance.get('/admin/relay/list');
+            console.log('response : ' + response);
+            setItemsRelay(response.data);
+        } catch (error) {
+            console.error('Error fetching relay : ', error);
         }
+    }
+
+    useEffect(() => {
         fetchRelays();
     }, []);
+
+    const handleDelete = async (id) => {
+        try {
+            await axiosInstance.patch(`/admin/relay/${id}`);
+            setItemsRelay(prevItems => prevItems.filter(item => item.relayId !== id));
+            fetchRelays();
+            console.log('Relay 종료 성공: ', id);
+        } catch (error) {
+            console.error('Relay 종료 실패: ', error);
+        }
+    };
 
     return (
         <>
@@ -38,8 +50,8 @@ const AdminRelayList = () => {
                     <TitleRectangle theme="subTitle">
                         <Text theme="text3">공모전 목록</Text>
                     </TitleRectangle>
-                    <TableWrapper>                        
-                        <Table items={items_relay} />
+                    <TableWrapper>
+                        {items_relay.length > 0 && <Table items={items_relay} type="relay" onDelete={handleDelete} />}
                     </TableWrapper>
                 </StyledRectangle>
             </Container>
