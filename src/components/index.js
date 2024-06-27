59ffcbcb90dd5f75804b8dde11f17f52dd71b2ba
JZU0 <ljzh1030@gmail.com>
2024-06-27 10:17:26 +0900

    💡 Comment : 컴포넌트 주석

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,40 @@
-export { default as Button } from './Button';
-export { default as Text } from './Text';
-export { default as NavMenu } from './NavMenu';
-export { default as Header } from './Header';
-export { default as Carousel } from './Carousel';
-export { default as Item } from './Item';
-export { default as Canvas } from './Canvas';
-export { default as OtherItem } from './OtherItem';
-export { default as OtherItemList } from './OtherItemList';
-export { default as Submission } from './Submission';
-export { default as Title } from './Title';
-export { default as Table } from './Table';
-export { default as Preview } from './Preview';
-export { default as Modal } from './Modal';
-export { default as MySubmissionModal } from './MySubmissionModal';
+// 관리자 페이지 헤더
+export { default as AdminHeader } from './AdminHeader';
+// 정보 알림/확인 모달
 export { default as AlertModal } from './AlertModal';
+// 버튼
+export { default as Button } from './Button';
+// 캔버스
+export { default as Canvas } from './Canvas';
+// 등록 페이지에서 이전 주차의 당선작들을 보기 위한 Carousel
+export { default as Carousel } from './Carousel';
+// 비밀번호 변경 모달
+export { default as ChangePasswordModal } from './ChangePasswordModal';
+// 서비스 푸터
 export { default as Footer } from './Footer';
+// 서비스 헤더
+export { default as Header } from './Header';
+// 입력창
+export { default as InputField } from './InputField';
+// Carousel 내부에 들어가는 컴포넌트
+export { default as Item } from './Item';
+// 관리자 페이지에서 사용하는 모달
+export { default as Modal } from './Modal';
+// 마이페이지에서 당선되지 않아 책뷰로 볼 수 없는 제출물 보여주는 모달
+export { default as MySubmissionModal } from './MySubmissionModal';
+// 헤더에 있는 네비게이션
+export { default as NavMenu } from './NavMenu';
+// 다른 사람 제출물 보기 위한 개별 컴포넌트
+export { default as OtherItem } from './OtherItem';
+// OtherItem을 모아둔 다른 사람 제출물 리스트 보는 컴포넌트
+export { default as OtherItemList } from './OtherItemList';
+// 진행중인 공모전 리스트 보기에 있는 컴포넌트
+export { default as Preview } from './Preview';
+// 마이페이지에서 제출물 리스트를 위한 컴포넌트
+export { default as Submission } from './Submission';
+// 공지사항이나 목록 뷰를 위한 테이블 컴포넌트
+export { default as Table } from './Table';
+// 테마별 글씨 재활용을 위한 컴포넌트
+export { default as Text } from './Text';
+// 페이지별 제목을 나타내주는 컴포넌트
+export { default as Title } from './Title';
